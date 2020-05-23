208e1d312636e805ded33b778223a67b904c65ff
Davide Di Criscito <davide.dicriscito@gmail.com>
2020-05-23 11:24:40 +0200

    fix(db): rinomina il database sqlite

--- minigest/settings.py
+++ minigest/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = "minigest.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "NAME": os.path.join(BASE_DIR, "minigest.sqlite3"),
     }
 }
 
