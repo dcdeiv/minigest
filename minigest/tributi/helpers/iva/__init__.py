3ebfefc5aa1d29110f2f88e26166b4c4ef6aafbd
Davide Di Criscito <davide.dicriscito@gmail.com>
2020-05-25 16:17:36 +0200

    fix(tributi): aggiunge il calcolo del lordo

--- minigest/tributi/helpers/iva/__init__.py
+++ minigest/tributi/helpers/iva/__init__.py
@@ -1,4 +1,11 @@
 from .aliquota import qanno, qdata, qmese
-from .calcolo_imp import calcolo_imponibile, calcolo_imposta
+from .calcolo_imp import calcolo_imponibile, calcolo_imposta, calcolo_totale
 
-__all__ = ["qanno", "qdata", "qmese", "calcolo_imposta", "calcolo_imponibile"]
+__all__ = [
+    "qanno",
+    "qdata",
+    "qmese",
+    "calcolo_imposta",
+    "calcolo_imponibile",
+    "calcolo_totale",
+]
--- minigest/tributi/helpers/iva/calcolo_imp.py
+++ minigest/tributi/helpers/iva/calcolo_imp.py
@@ -1,19 +1,31 @@
 from decimal import Decimal
 
 
-def calcolo_imponibile(totale, aliquota):
-    totale = Decimal(totale)
-    aliquota = Decimal(aliquota)
+def calcolo_imponibile(lordo, aliquota):
+    base = Decimal(100)
+    purged_aliquota = Decimal(aliquota)
+    purged_lordo = Decimal(lordo)
 
-    imponibile = (totale * 100) / (100 + aliquota)
+    imponibile = (purged_lordo * base) / (base + purged_aliquota)
 
-    return Decimal(round(imponibile, 2))
+    return Decimal(imponibile)
 
 
-def calcolo_imposta(totale, aliquota):
-    totale = Decimal(totale)
-    aliquota = Decimal(aliquota)
+def calcolo_imposta(lordo, aliquota):
+    base = Decimal(100)
+    purged_aliquota = Decimal(aliquota)
+    purged_lordo = Decimal(lordo)
 
-    imposta = (totale * aliquota) / (100 + aliquota)
+    imposta = (purged_lordo * aliquota) / (base + purged_aliquota)
 
-    return Decimal(round(imposta, 2))
+    return Decimal(imposta)
+
+
+def calcolo_totale(imponibile, aliquota):
+    base = Decimal(100)
+    purged_imponibile = Decimal(imponibile)
+    purged_aliquota = Decimal(aliquota)
+
+    totale = purged_imponibile * (purged_aliquota + base) / base
+
+    return Decimal(totale)
