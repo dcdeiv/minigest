06a16befd9c706fc237cac9b0d5b6fcd11667d90
Davide Di Criscito <davide.dicriscito@gmail.com>
2020-05-29 13:50:33 +0200

    feat(pdf): aggiunge gli url di pdf

--- minigest/urls.py
+++ minigest/urls.py
@@ -5,4 +5,5 @@ from django.contrib import admin
 urlpatterns = [
     path("admin/", admin.site.urls, name="admin"),
     path("api/", include("rest.urls"), name="api"),
+    path("pdf/", include("pdf.urls"), name="pdf"),
 ]
