2484095f72e34ed1b00d263e2ab5f374ea5121e2
Davide Di Criscito <davide.dicriscito@gmail.com>
2020-06-02 13:24:43 +0200

    feat: inizializza redux

--- webclient/src/index.js
+++ webclient/src/index.js
@@ -1,25 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider as ReduxProvider } from "react-redux";
+import { IntlProvider } from "react-intl";
+import { Router, Switch, Route } from "react-router-dom";
 import { CssBaseline } from "@material-ui/core";
 import { ThemeProvider } from "@material-ui/core/styles";
-import { Router, Switch, Route } from "react-router-dom";
-import { history } from "~/helpers";
 
 // Local
-import App, { NotFound } from "~/App";
 import * as serviceWorker from "~/serviceWorker";
 import { theme } from "~/Layout";
+import { history } from "~/helpers";
+import { store } from "~/store";
+
+// App
+import App from "~/App";
+import NotFound from "~/NotFound";
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <Router history={history}>
-      <Switch>
-        <Route exact path="/" component={App} />
-        <Route component={NotFound} />
-      </Switch>
-    </Router>
-  </ThemeProvider>,
+  <IntlProvider locale="it">
+    <ReduxProvider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router history={history}>
+          <Switch>
+            <Route exact path="/" component={App} />
+            <Route component={NotFound} />
+          </Switch>
+        </Router>
+      </ThemeProvider>
+    </ReduxProvider>
+  </IntlProvider>,
   document.getElementById("root")
 );
 
---
+++ webclient/src/store/reducer.js
@@ -0,0 +1,4 @@
+import { combineReducers } from "redux";
+
+// RootReducer
+export default combineReducers({});
