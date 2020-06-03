daa2613bd455caedb9fa8ab763bbcde83ed57b6f
Davide Di Criscito <davide.dicriscito@gmail.com>
2020-06-04 00:22:13 +0200

    feat(webclient): aggiunge ListaVuota, LoadingSpinner e Percentuale

--- webclient/src/Components/index.js
+++ webclient/src/Components/index.js
@@ -1,5 +1,15 @@
 import AdapterLink from "./AdapterLink";
+import ListaVuota from "./ListaVuota";
 import ListItemLink from "./ListItemLink";
+import LoadingSpinner from "./LoadingSpinner";
+import Percentuale from "./Percentuale";
 import Transition from "./Transition";
 
-export { AdapterLink, ListItemLink, Transition };
+export {
+  AdapterLink,
+  ListaVuota,
+  ListItemLink,
+  LoadingSpinner,
+  Percentuale,
+  Transition,
+};
