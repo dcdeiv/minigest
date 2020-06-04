a3c3cc369b2bfcc1841c7d7a6c06e0102be169eb
Davide Di Criscito <davide.dicriscito@gmail.com>
2020-06-04 23:40:06 +0200

    feat(webclient): aggiunge MuiPickersUtilsProvider e imposta locale su it sia per moment che per intl

--- webclient/src/index.js
+++ webclient/src/index.js
@@ -1,22 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import moment from "moment";
 import { Provider as ReduxProvider } from "react-redux";
 import { IntlProvider } from "react-intl";
 import { CssBaseline } from "@material-ui/core";
 import { ThemeProvider } from "@material-ui/core/styles";
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
+import MomentUtils from "@date-io/moment";
 import * as serviceWorker from "~/serviceWorker";
 import { theme } from "~/Layout";
 import { store } from "~/store";
 import App from "~/App";
+import "moment/locale/it";
+
+moment.locale("it");
 
 ReactDOM.render(
-  <IntlProvider locale={navigator.language}>
-    <ReduxProvider store={store}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
-    </ReduxProvider>
+  <IntlProvider locale="it">
+    <MuiPickersUtilsProvider
+      libInstance={moment}
+      utils={MomentUtils}
+      locale="it"
+    >
+      <ReduxProvider store={store}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+      </ReduxProvider>
+    </MuiPickersUtilsProvider>
   </IntlProvider>,
   document.getElementById("root")
 );
