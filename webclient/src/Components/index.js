8b8d34adae714ea8798f6516f73083800cb6bb91
Davide Di Criscito <davide.dicriscito@gmail.com>
2020-06-04 17:31:59 +0200

    feat(webclient): aggiunge il componente FabFixedContainer

--- webclient/src/Components/index.js
+++ webclient/src/Components/index.js
@@ -1,5 +1,6 @@
 import AdapterLink from "./AdapterLink";
 import FabFixed from "./FabFixed";
+import FabFixedContainer from "./FabFixedContainer";
 import ListaVuota from "./ListaVuota";
 import ListItemLink from "./ListItemLink";
 import LoadingSpinner from "./LoadingSpinner";
@@ -9,6 +10,7 @@ import Transition from "./Transition";
 export {
   AdapterLink,
   FabFixed,
+  FabFixedContainer,
   ListaVuota,
   ListItemLink,
   LoadingSpinner,
