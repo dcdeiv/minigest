365ccca782d6db6d59c3a3b1717011e55e401def
Davide Di Criscito <davide.dicriscito@gmail.com>
2020-06-05 14:12:29 +0200

    test(webclient): testa lo script collectclient

---
+++ webclient/collectclient.py
@@ -0,0 +1,157 @@
+import json
+import os
+import re
+import shutil
+
+CLIENT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+TEMPLATE_DIR = os.path.join(CLIENT_DIR, "templates/webclient")
+INDEX_OLD = os.path.join(TEMPLATE_DIR, "index.html")
+
+STATIC_DIR = os.path.join(CLIENT_DIR, "static/webclient")
+
+BUILD_DIR = os.path.join(CLIENT_DIR, "build")
+INDEX_NEW = os.path.join(BUILD_DIR, "index.html")
+ASSET_MANIFEST = os.path.join(BUILD_DIR, "asset-manifest.json")
+
+
+def index():
+    if os.path.exists(INDEX_OLD):
+        # Elimina il vecchio index.html
+        os.remove(INDEX_OLD)
+    else:
+        shutil.move(INDEX_NEW, INDEX_OLD)
+        print("- index.html copiato!")
+        print("")
+
+
+def asset_manifest():
+    print("- modifica di asset-manifest.json")
+
+    # Modifica asset-manifest.json
+    with open(ASSET_MANIFEST, "r+") as f:
+        data = json.load(f)
+
+        # aggiorna la posizione di index.html
+        data["files"]["index.html"] = "/index.html"
+        f.seek(0)
+        json.dump(data, f, indent=2)
+        f.truncate()
+    print("- asset-manifest.json modificato!")
+    print("")
+    return 1
+
+
+def service_worker():
+    print(" - modifica di service-worker.js")
+
+    SERVICE_OUT = os.path.join(BUILD_DIR, "service-worker.js")
+    SERVICE_BACKUP = os.path.join(BUILD_DIR, "service-worker-back.js")
+
+    print("- creazione del backup di service-worker.js")
+    shutil.copy2(SERVICE_OUT, SERVICE_BACKUP)
+
+    print("- modifica di service-worker.js")
+
+    R_SERVICE = open(SERVICE_BACKUP, "rt")
+    W_SERVICE = open(SERVICE_OUT, "wt")
+
+    for line in R_SERVICE:
+        W_SERVICE.write(line.replace('"/static/webclient/index.html"', '"/index.html"'))
+
+    print("- service-worker.js modificato!")
+
+    # eliminazione del backup
+    print("- eliminazione del backup\n")
+    os.remove(SERVICE_BACKUP)
+    return 1
+
+
+def precache_manifest():
+    # Cerca il precache-manifest
+    print("- ricerca di precache-manifest.(.*).js")
+
+    pattern = re.compile(r"^precache-manifest[\.][a-zA-Z0-9]*[\.]js$")
+    MANIFEST_NAME = ""
+
+    # Ricerca del manifest
+    for f in os.listdir(BUILD_DIR):
+        if not pattern.match(f):
+            pass
+        else:
+            MANIFEST_NAME = f
+
+    if MANIFEST_NAME == "":
+        print("Errore: precache-manifest.(.*).js non trovato!")
+        return 0
+    else:
+        print(f"- trovato: {MANIFEST_NAME}")
+        print(f"- creazione del backup di {MANIFEST_NAME}")
+
+        MANIFEST_OUT = os.path.join(BUILD_DIR, MANIFEST_NAME)
+        MANIFEST_BACKUP = os.path.join(BUILD_DIR, "precache-manifest.js")
+
+        shutil.copy2(MANIFEST_OUT, MANIFEST_BACKUP)
+
+        print(f"- modifica di {MANIFEST_NAME}")
+        R_MANIFEST = open(MANIFEST_BACKUP, "rt")
+        W_MANIFEST = open(MANIFEST_OUT, "wt")
+
+        for line in R_MANIFEST:
+            W_MANIFEST.write(
+                line.replace('"/static/webclient/index.html"', '"/index.html"')
+            )
+
+        print(f"- {MANIFEST_NAME} modificato!")
+
+        # eliminazione del backup
+        print("- eliminazione del backup\n")
+        os.remove(MANIFEST_BACKUP)
+        return 1
+
+
+def collectstatics():
+    print("- copia dei file")
+
+    # Eliminazione dei vecchi file
+    if os.path.isdir(STATIC_DIR):
+        shutil.rmtree(STATIC_DIR)
+
+    # Copia dei file
+    os.rename(BUILD_DIR, STATIC_DIR)
+
+    print("- copia dei file terminata!")
+    print("")
+    return 1
+
+
+def run():
+    # Controlla che la cartella "build" esista
+    if not os.path.isdir(BUILD_DIR):
+        messaggio = """
+        Client non costruito!\n
+        Per costruirlo esegui il comando yarn run build:webclient
+        """
+        print(messaggio)
+    else:
+        # Controlla se la cartella "build" è vuota
+        if not os.listdir(BUILD_DIR):
+            messaggio = """
+            Client non costruito!\n
+            Sembra che la cartella webclient/build sia vuota!
+            Sei sicuro di aver dato il comando yarn run build:webclient
+            e che questo abbia finito?
+            """
+            print(messaggio)
+        else:
+            # Copia del file index
+            index()
+
+            # Modifica del file asset-manifest.json
+            asset_manifest()
+
+            # Modifica del file precache-manifest
+            precache_manifest()
+
+            # Sposta e rinomina la cartella build in ./static/webclient
+            collectstatics()
--- webclient/static/webclient/service-worker.js
+++ webclient/static/webclient/service-worker.js
@@ -11,16 +11,14 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
-);
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/static/webclient/precache-manifest.c504b908dcf57cd839d7d6b32962573f.js"
+  "/static/webclient/precache-manifest.bdea283af0a77130084ab845021d5b06.js"
 );
 
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
 });
@@ -35,9 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(
-  workbox.precaching.getCacheKeyForURL("index.html"),
-  {
-    blacklist: [/^\/_/, /\/[^/?]+\.[^/]+$/],
-  }
-);
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/static/webclient/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
