23c4c3d660f698abe304eff3804031cbb84a2580
Davide Di Criscito <davide.dicriscito@gmail.com>
2020-06-26 12:03:04 +0200

    docs: aggiunge AppHeader

--- packages/docs/src/App/Home.jsx
+++ packages/docs/src/App/Home.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { Typography } from "@material-ui/core";
+import { AppHeader } from "src/Components";
 
 export function Home(props) {
   return (
     <React.Fragment>
-      <Typography component="h1" variant="h3" gutterBottom>
-        minigest
-      </Typography>
+      <AppHeader>
+        <Typography component="h1" variant="h3">
+          minigest
+        </Typography>
+        <Typography variant="subtitle1">
+          Un mini gestionale pensato per aiutare a mantenere i conti di
+          un'azienda
+        </Typography>
+      </AppHeader>
+
       <Typography>in costruzione...</Typography>
     </React.Fragment>
   );
