a9be12f338f1268a96cf938abf97f8a8b713d190
Davide Di Criscito <davide.dicriscito@gmail.com>
2020-06-26 18:05:12 +0200

    docs: completa la sezione pypi

---
+++ packages/docs/src/App/Installazione/PyPi/index.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Typography, Divider, Box } from "@material-ui/core";
+import { AppHeader, CodeBlock } from "src/Components";
+
+// Sezioni
+import { Descrizione } from "./Descrizione";
+import { Dipendenze } from "./Dipendenze";
+import { Applicazioni } from "./Applicazioni";
+import { Configurazione } from "./Configurazione";
+import { Fine } from "./Fine";
+
+export function PyPi(props) {
+  return (
+    <React.Fragment>
+      <AppHeader>
+        <Typography variant="overline">Installazione</Typography>
+        <Typography variant="h3">PyPi</Typography>
+
+        <Box mt={4}>
+          <Box mb={4}>
+            <Divider />
+          </Box>
+          <CodeBlock mb={4}>pip install minigest</CodeBlock>
+        </Box>
+      </AppHeader>
+
+      <Descrizione id="installazione-pypi-descrizione" />
+
+      <Dipendenze id="installazione-pypi-dipendenze" />
+
+      <Applicazioni id="installazione-pypi-applicazioni" />
+
+      <Configurazione id="installazione-pypi-configurazione" />
+
+      <Fine id="installazione-pypi-fine" />
+    </React.Fragment>
+  );
+}
