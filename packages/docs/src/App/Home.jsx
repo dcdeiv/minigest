3028869cc3a68497b5482fb3dd71bb9ccc14390b
Davide Di Criscito <davide.dicriscito@gmail.com>
2020-06-26 11:25:54 +0200

    docs: sposta il contenuto di App/index.jsx in Home.jsx

---
+++ packages/docs/src/App/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+
+export function Home(props) {
+  return (
+    <React.Fragment>
+      <Typography component="h1" variant="h3" gutterBottom>
+        minigest
+      </Typography>
+      <Typography>in costruzione...</Typography>
+    </React.Fragment>
+  );
+}
