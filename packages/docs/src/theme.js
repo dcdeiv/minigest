89151ef9048e66fbfaace7aa326e8572f52f5e40
Davide Di Criscito <davide.dicriscito@gmail.com>
2020-06-26 10:44:46 +0200

    docs: copia il tema

---
+++ packages/docs/src/theme.js
@@ -0,0 +1,13 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  common: {
+    white: "#F8F8F8",
+    black: "#2B2B2B",
+  },
+  palette: {
+    type: "dark",
+  },
+});
+
+export default theme;
