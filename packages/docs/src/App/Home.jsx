4100945cfad10e37bb95141f027d076119195af5
Davide Di Criscito <davide.dicriscito@gmail.com>
2020-06-26 12:41:50 +0200

    docs(Home): aggiunge le azioni per la documentazione PyPi e Docker

--- packages/docs/src/App/Home.jsx
+++ packages/docs/src/App/Home.jsx
@@ -1,21 +1,76 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
+import {
+  Typography,
+  Box,
+  Grid,
+  Card,
+  CardHeader,
+  CardActions,
+  Button,
+  Divider,
+} from "@material-ui/core";
 import { AppHeader } from "src/Components";
+import WidgetsIcon from "@material-ui/icons/Widgets";
+import DiscFullIcon from "@material-ui/icons/DiscFull";
 
 export function Home(props) {
   return (
     <React.Fragment>
       <AppHeader>
-        <Typography component="h1" variant="h3">
+        <Typography variant="h1" gutterBottom>
           minigest
         </Typography>
-        <Typography variant="subtitle1">
-          Un mini gestionale pensato per aiutare a mantenere i conti di
-          un'azienda
+        <Typography>
+          Minigest è un'applicazione scritta in python per il framework django,
+          completamente gratuita e distribuita con la licenza MIT. È rivolta a
+          tutte le piccole aziende che hanno bisogno di un programma che
+          permetta loro di visualizzare immediamente la loro situazione
+          contabile, di tenere traccia dei pagamenti, degli acquisti, delle
+          vendite e delle tasse e imposte da pagare.
         </Typography>
       </AppHeader>
 
-      <Typography>in costruzione...</Typography>
+      <Box mb={3}>
+        <Grid
+          container
+          direction="row"
+          justify="space-between"
+          alignItems="center"
+          spacing={2}
+        >
+          <Grid item xs={12} sm={6}>
+            <Card>
+              <CardHeader
+                avatar={<WidgetsIcon />}
+                title="PyPi"
+                subheader="guida all'installazione di minigest attraverso PyPi"
+              />
+              <CardActions>
+                <Button variant="contained" color="primary">
+                  continua
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <Card>
+              <CardHeader
+                avatar={<DiscFullIcon />}
+                title="Docker"
+                subheader="guida all'installazione di minigest attraverso Docker"
+              />
+              <CardActions>
+                <Button variant="contained" color="primary">
+                  continua
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
+      </Box>
+
+      <Divider />
     </React.Fragment>
   );
 }
