47d502a345392badef4a67052b2c796415260a46
Davide Di Criscito <davide.dicriscito@gmail.com>
2020-06-26 10:58:12 +0200

    docs: aggiunge IntlProvider e ThemeProvider

--- packages/docs/src/index.js
+++ packages/docs/src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { IntlProvider } from "react-intl";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { CssBaseLine } from "@material-ui/core";
 import { App } from "./App";
+import { theme } from "./theme";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <IntlProvider locale="it">
+    <ThemeProvider theme={theme}>
+      <CssBaseLine />
+      <App />
+    </ThemeProvider>
+  </IntlProvider>,
   document.getElementById("root")
 );
