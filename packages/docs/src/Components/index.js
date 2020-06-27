2e6f808ce55db68699086ff9f4b73e3a6c97cd19
Davide Di Criscito <davide.dicriscito@gmail.com>
2020-06-27 18:09:44 +0200

    docs: aggiunge Image

--- packages/docs/src/Components/index.js
+++ packages/docs/src/Components/index.js
@@ -2,6 +2,7 @@ export { Layout } from "./Layout";
 export { AdapterLink } from "./AdapterLink";
 export { AppHeader } from "./AppHeader";
 export { CodeBlock } from "./CodeBlock";
+export { Image } from "./Image";
 export { ListItemLink } from "./ListItemLink";
 export { LoadingSpinner } from "./LoadingSpinner";
 export { Section, HeaderSection } from "./Section";
