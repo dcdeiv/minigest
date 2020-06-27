b2840c38b6811f67fab760887ca60efd6a4a67b3
Davide Di Criscito <davide.dicriscito@gmail.com>
2020-06-27 16:53:14 +0200

    docs: aggiunge SetUpWidget in Installazione

--- packages/docs/src/App/Installazione/index.jsx
+++ packages/docs/src/App/Installazione/index.jsx
@@ -1,18 +1,34 @@
 import React from "react";
 import { Switch, Route, useRouteMatch } from "react-router-dom";
+import { Typography } from "@material-ui/core";
+import { AppHeader } from "src/Components";
 
 // Routes
 import { Docker } from "./Docker";
 import { PyPi } from "./PyPi";
 
 // Widgets
-export { SetUpWidget } from "./Widget";
+import { SetUpWidget } from "./Widget";
+export { SetUpWidget };
 
+// Home
 export function Installazione(props) {
   let { path } = useRouteMatch();
 
   return (
     <Switch>
+      <Route exact path={path}>
+        <React.Fragment>
+          <AppHeader mb={3}>
+            <Typography variant="h3">Installazione</Typography>
+            <Typography variant="subtitle1">
+              Scegli come installare minigest
+            </Typography>
+          </AppHeader>
+
+          <SetUpWidget />
+        </React.Fragment>
+      </Route>
       <Route path={`${path}/pypi`} component={PyPi} />
       <Route path={`${path}/docker`} component={Docker} />
     </Switch>
