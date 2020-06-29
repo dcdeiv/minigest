5c9347695dfc98e7d34e1c3db731e7ff05f6716e
Davide Di Criscito <davide.dicriscito@gmail.com>
2020-06-29 16:45:42 +0200

    docs: elimina gli import non utilizzati

--- packages/docs/src/App/Installazione/index.jsx
+++ packages/docs/src/App/Installazione/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch, Route, useRouteMatch } from "react-router-dom";
 import { Typography } from "@material-ui/core";
-import { AppHeader, Section, HeaderSection } from "src/Components";
+import { AppHeader } from "src/Components";
 
 // Routes
 import { Docker } from "./Docker";
