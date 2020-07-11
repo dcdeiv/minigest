a4912d2aa337c68a1d75030ae297566f163876fb
Davide Di Criscito <davide.dicriscito@gmail.com>
2020-07-11 22:18:37 +0200

    feat(www): aggiunge Layout

--- packages/www/src/App/index.jsx
+++ packages/www/src/App/index.jsx
@@ -1,5 +1,99 @@
 import React from "react";
+import { Layout } from "@minigest/ui";
 
 export function App() {
-  return <p>Ciao!</p>;
+  return (
+    <Layout>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+        eligendi pariatur possimus, in neque officia! Ea assumenda, sunt unde id
+        nobis praesentium eum tempore maiores doloremque dolor quisquam voluptas
+        eligendi?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+        eligendi pariatur possimus, in neque officia! Ea assumenda, sunt unde id
+        nobis praesentium eum tempore maiores doloremque dolor quisquam voluptas
+        eligendi?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+        eligendi pariatur possimus, in neque officia! Ea assumenda, sunt unde id
+        nobis praesentium eum tempore maiores doloremque dolor quisquam voluptas
+        eligendi?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+        eligendi pariatur possimus, in neque officia! Ea assumenda, sunt unde id
+        nobis praesentium eum tempore maiores doloremque dolor quisquam voluptas
+        eligendi?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+        eligendi pariatur possimus, in neque officia! Ea assumenda, sunt unde id
+        nobis praesentium eum tempore maiores doloremque dolor quisquam voluptas
+        eligendi?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+        eligendi pariatur possimus, in neque officia! Ea assumenda, sunt unde id
+        nobis praesentium eum tempore maiores doloremque dolor quisquam voluptas
+        eligendi?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+        eligendi pariatur possimus, in neque officia! Ea assumenda, sunt unde id
+        nobis praesentium eum tempore maiores doloremque dolor quisquam voluptas
+        eligendi?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+        eligendi pariatur possimus, in neque officia! Ea assumenda, sunt unde id
+        nobis praesentium eum tempore maiores doloremque dolor quisquam voluptas
+        eligendi?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+        eligendi pariatur possimus, in neque officia! Ea assumenda, sunt unde id
+        nobis praesentium eum tempore maiores doloremque dolor quisquam voluptas
+        eligendi?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+        eligendi pariatur possimus, in neque officia! Ea assumenda, sunt unde id
+        nobis praesentium eum tempore maiores doloremque dolor quisquam voluptas
+        eligendi?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+        eligendi pariatur possimus, in neque officia! Ea assumenda, sunt unde id
+        nobis praesentium eum tempore maiores doloremque dolor quisquam voluptas
+        eligendi?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+        eligendi pariatur possimus, in neque officia! Ea assumenda, sunt unde id
+        nobis praesentium eum tempore maiores doloremque dolor quisquam voluptas
+        eligendi?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+        eligendi pariatur possimus, in neque officia! Ea assumenda, sunt unde id
+        nobis praesentium eum tempore maiores doloremque dolor quisquam voluptas
+        eligendi?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+        eligendi pariatur possimus, in neque officia! Ea assumenda, sunt unde id
+        nobis praesentium eum tempore maiores doloremque dolor quisquam voluptas
+        eligendi?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+        eligendi pariatur possimus, in neque officia! Ea assumenda, sunt unde id
+        nobis praesentium eum tempore maiores doloremque dolor quisquam voluptas
+        eligendi?
+      </p>
+    </Layout>
+  );
 }
