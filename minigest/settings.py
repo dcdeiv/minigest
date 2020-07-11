75446d2daa1d4a1a5149e432234d27e85a1cf223
Davide Di Criscito <davide.dicriscito@gmail.com>
2020-07-11 11:38:16 +0200

    docs(minigest): aggiunge un commento a MINIGEST_APPS

--- minigest/settings.py
+++ minigest/settings.py
@@ -1,1 +1,7 @@
+"""
+    Importa MINIGEST_APPS per installare tutte le applicazioni
+    minigest in una sola volta!
+    Altrimenti usa questa impostazione come riferimento per le app
+    da installare
+"""
 MINIGEST_APPS = ["minigest.account"]
