185d5f63792dcd87eecc4e11a8edfc12ac230f34
Davide Di Criscito <davide.dicriscito@gmail.com>
2020-07-11 19:30:28 +0200

    feat(webapp): aggiuge l'url a www

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,8 +1,9 @@
-from django.urls import include, path
+from django.urls import include, path, re_path
 
 from django.contrib import admin
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("minigest.rest.urls")),
+    re_path(r"", include("minigest.www.urls")),
 ]
