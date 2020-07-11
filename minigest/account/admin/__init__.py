09a2f119696c14322a96b8008edef7f02dd1e5a8
Davide Di Criscito <davide.dicriscito@gmail.com>
2020-07-11 11:35:23 +0200

    style: formattato

--- minigest/account/admin/__init__.py
+++ minigest/account/admin/__init__.py
@@ -1,7 +1,7 @@
+from .utente import UtenteAdmin
+
 from django.contrib import admin
 from django.contrib.auth.models import Group
-
-from .utente import UtenteAdmin
 
 admin.site.unregister(Group)
 
--- minigest/settings.py
+++ minigest/settings.py
@@ -1,2 +1,1 @@
 MINIGEST_APPS = ["minigest.account"]
-
