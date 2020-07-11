e629472773c2d55542126ea2cb3164146f491f6a
Davide Di Criscito <davide.dicriscito@gmail.com>
2020-07-11 22:59:47 +0200

    fix: cambia defaltTheme in theme

--- packages/ui/src/theme.js
+++ packages/ui/src/theme.js
@@ -1,6 +1,6 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 
-export const defaultTheme = createMuiTheme({
+export const theme = createMuiTheme({
   common: {
     white: "#F8F8F8",
     black: "#2B2B2B",
