ac2ed5824b5a0192e9e2e31bd7d645ce9ad55404
Davide Di Criscito <davide.dicriscito@gmail.com>
2020-07-11 14:09:22 +0200

    refactor(www): cerca di installare @minigest/helpers ma c'è un problema con yarn

--- packages/www/src/index.js
+++ packages/www/src/index.js
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "@material-ui/styles";
 import { App } from "./App";
+import { defaultTheme } from "@minigest/helpers";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ThemeProvider theme={defaultTheme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
