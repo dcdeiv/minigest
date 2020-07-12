ed4a78859eaf0eb30f99d563c1e2b8e9bbfe7e0a
Davide Di Criscito <davide.dicriscito@gmail.com>
2020-07-12 19:55:48 +0200

    fix(ui): cambia l'header in Profilo/Scheda

--- packages/www/src/App/Profilo/Scheda.jsx
+++ packages/www/src/App/Profilo/Scheda.jsx
@@ -1,11 +1,29 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/Styles";
 import { AppHeader, AppContent } from "@minigest/ui";
 
+const useStyles = makeStyles((theme) => ({
+  alertNome: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
 export function Scheda() {
+  const classes = useStyles();
+  let { dettagli: user } = useSelector((state) => state.utente.utente);
+
   return (
     <React.Fragment>
       <AppHeader>
-        <p>ciao</p>
+        <Typography variant="h2">Ciao, {user.nome || "Utente"}!</Typography>
+        {!user.nome || !user.cognome ? (
+          <Typography variant="body2" className={classes.alertNome}>
+            Non hai fornito il tuo nome o il tuo cognome! Inseriscili per
+            visualizzarli!
+          </Typography>
+        ) : null}
       </AppHeader>
       <AppContent>
         <p>
