b09ac32fe7300fb78f880bb2e4776e0045c2e0aa
Davide Di Criscito <davide.dicriscito@gmail.com>
2020-07-12 16:54:57 +0200

    feat(www): aggiunge il componente LoadingApp per bloccare l'accesso all'applicazione quando non si è loggati

--- packages/www/src/App/index.jsx
+++ packages/www/src/App/index.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { Switch, Route, useRouteMatch } from "react-router-dom";
-import { Layout } from "@minigest/ui";
+import { useDispatch, useSelector } from "react-redux";
+import { Typography } from "@material-ui/core";
+import { Layout, LoadingApp, MessageBox, Splash } from "@minigest/ui";
 import { AppDrawer } from "src/Components";
+import { action } from "src/state";
 
 // External Routes
 import Accedi from "./Accedi.jsx";
@@ -11,13 +14,37 @@ import { Home } from "./Home.jsx";
 
 export { Accedi };
 export function App() {
+  const dispatch = useDispatch();
+  let { id } = useSelector((state) => state.utente.auth);
+  let { getting, getError } = useSelector((state) => state.utente.utente);
   let { path } = useRouteMatch();
 
-  return (
-    <Layout drawer={AppDrawer}>
-      <Switch>
-        <Route exact path={path} component={Home} />
-      </Switch>
-    </Layout>
-  );
+  // Scarica i dettagli dell'utente
+  React.useEffect(() => {
+    if (id) {
+      dispatch(action.utente.get(id));
+    }
+  }, [id, dispatch]);
+
+  if (getting) {
+    return <LoadingApp />;
+  } else {
+    if (getError || id === false) {
+      return (
+        <Splash>
+          <MessageBox variant="error">
+            <Typography>{getError}</Typography>
+          </MessageBox>
+        </Splash>
+      );
+    } else {
+      return (
+        <Layout drawer={AppDrawer}>
+          <Switch>
+            <Route exact path={path} component={Home} />
+          </Switch>
+        </Layout>
+      );
+    }
+  }
 }
