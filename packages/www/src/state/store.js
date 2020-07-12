5e3c527c338e89c6fe012f5b8d706059caf81688
Davide Di Criscito <davide.dicriscito@gmail.com>
2020-07-12 17:40:54 +0200

    build(www): installa il dev tool di redux

--- packages/www/src/state/store.js
+++ packages/www/src/state/store.js
@@ -2,4 +2,6 @@ import { createStore, compose } from "redux";
 import rootReducer from "./reducer";
 import middleware from "./middleware";
 
-export default compose(middleware)(createStore)(rootReducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default composeEnhancers(middleware)(createStore)(rootReducer);
