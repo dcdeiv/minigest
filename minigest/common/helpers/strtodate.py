30b04d1c0042d0e5d2667b045b104f2a44e22e19
Davide Di Criscito <davide.dicriscito@gmail.com>
2020-07-17 16:33:03 +0200

    elimina common

--- minigest/common/helpers/strtodate.py
+++
@@ -1,9 +0,0 @@
-from datetime import date
-
-
-def strtodate(data):
-    if isinstance(data, date):
-        return data
-    else:
-        data = data.split("-")
-        return date(int(data[0]), int(data[1]), int(data[2]))
