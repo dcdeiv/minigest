5244b679ace4047c8c2e0796d5f1a4e7eb4db987
Davide Di Criscito <davide.dicriscito@gmail.com>
2020-07-19 10:32:01 +0200

    fix(minigest): aggiunge il serializer delle imprese in Utente

--- minigest/account/serializers/utente.py
+++ minigest/account/serializers/utente.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
 from minigest.account.models import Utente
+from minigest.anagrafica.serializers import ImpresaRelazioneSerializer
 
 
 class UtenteSerializer(serializers.ModelSerializer):
+    imprese = ImpresaRelazioneSerializer(many=True, read_only=True)
+
     class Meta:
         model = Utente
         fields = [
@@ -13,5 +16,6 @@ class UtenteSerializer(serializers.ModelSerializer):
             "email",
             "cognome",
             "nome",
+            "imprese",
         ]
-        read_only_fields = ["id", "is_staff", "is_authenticated"]
+        read_only_fields = ["id", "is_staff", "is_authenticated", "imprese"]
