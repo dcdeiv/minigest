e2829c5c782fd886d2218da2348ebead871a0c51
Davide Di Criscito <davide.dicriscito@gmail.com>
2020-07-20 19:44:02 +0200

    fix(www): elimina il prop name

--- packages/www/src/Components/InputField.jsx
+++ packages/www/src/Components/InputField.jsx
@@ -2,19 +2,21 @@ import React from "react";
 import { TextField } from "@material-ui/core";
 
 export default function InputField(props) {
-  let { name, options } = props;
-
-  console.log(options);
+  let { options, onChange } = props;
 
   if (options.type === "string") {
     return (
       <TextField
         fullWidth
         variant="outlined"
-        inputProps={{ name: name }}
+        inputProps={{ name: options.name }}
         helperText={options.help_text}
         label={options.label}
         required={options.required}
+        error={options.error}
+        onChange={onChange}
+        disabled={options.disabled || false}
+        value={options.value}
       />
     );
   }
